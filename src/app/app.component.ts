bb4469604425c4661361e4bde13d3ba853fe447c
Marouane Cherif <marouane.cherif1@esprit.tn>
2023-05-31 16:46:00 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var StripeTerminal: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stripeFront';
+  terminal: any;
+  amount: number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  // Initialisez le SDK Stripe Terminal
+  ngOnInit() {
+    this.terminal = StripeTerminal.create({
+      onFetchConnectionToken: this.fetchConnectionToken,
+      onUnexpectedReaderDisconnect: this.unexpectedDisconnect
+    });
+  }
+
+  // Fonction pour récupérer le ConnectionToken depuis votre backend
+  fetchConnectionToken = () => {
+    return this.http.post('/api/connection_token', {}).toPromise()
+      .then((response: any) => {
+        return response.secret;
+      });
+  }
+
+  // Fonction pour gérer les déconnexions inattendues du lecteur
+  unexpectedDisconnect = () => {
+    console.log('Déconnexion inattendue du lecteur');
+  }
+
+  discoverReaders() {
+    this.terminal.discoverReaders({ simulated: true }).then((result: any) => {
+      if (result.error) {
+        console.log('Échec de la détection du lecteur : ', result.error);
+      } else if (result.discoveredReaders.length === 0) {
+        console.log('Aucun lecteur disponible.');
+      } else {
+        const selectedReader = result.discoveredReaders[0];
+        this.terminal.connectReader(selectedReader).then((connectResult: any) => {
+          if (connectResult.error) {
+            console.log('Échec de la connexion au lecteur : ', connectResult.error);
+          } else {
+            console.log('Connecté au lecteur : ', connectResult.reader.label);
+          }
+        });
+      }
+    });
+  }
+
+
+  collectPayment(amount: number) {
+    this.fetchPaymentIntentClientSecret(amount).then((client_secret: string) => {
+      this.terminal.setSimulatorConfiguration({ testCardNumber: '4242424242424242' });
+      this.terminal.collectPaymentMethod(client_secret).then((result: any) => {
+        if (result.error) {
+          // Gérez les erreurs
+        } else {
+          this.terminal.processPayment(result.paymentIntent).then((result: any) => {
+            if (result.error) {
+              console.log(result.error);
+            } else if (result.paymentIntent) {
+              const paymentIntentId = result.paymentIntent.id;
+              // Traitez le paiement
+            }
+          });
+        }
+      });
+    });
+  }
+
+  fetchPaymentIntentClientSecret(amount: number) {
+    const bodyContent = { amount: amount };
+    return this.http.post('/api/create_payment_intent', bodyContent).toPromise()
+      .then((response: any) => {
+        return response.client_secret;
+      });
+  }
+
 }
